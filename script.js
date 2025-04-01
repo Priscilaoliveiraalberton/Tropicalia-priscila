da71b093280fbcaf7e13528cfab5583682fe8bc3
Priscila Oliveira Alberton <145785226+Priscilaoliveiraalberton@users.noreply.github.com>
2025-04-01 08:48:27 -0300

    01/04

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementBy('diminui-fonte);
 
     let tamanhoAtualFonte = 1; 
 
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-        
     });
 });
